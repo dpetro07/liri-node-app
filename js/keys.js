01cafe57111b08014a65ebb37c8baf7b97a853bc
Daniel Petro <DanielPetro@Daniels-MacBook-Pro.local>
2016-01-16 09:08:09 -0500

    added twitter access tokens and consumer keys and made variables for liri.js

--- js/keys.js
+++ js/keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: ' WPlYxORD0QmgOopmexMO743oF',
+  consumer_secret: '2p1YOb0L3KklzuPgqfpBuIRfM3p2oBk6IdVwDnsqnb07MSJQYK',
+  access_token_key: '4680090378-Pi1go3W6KqiQBiRQrfjSBt2JQPCJTwEjSywYxwt',
+  access_token_secret: 'GGCP14E8XZDoutdH0HTBd1RSjPD4roahECuX43DAttzvX',
 }
