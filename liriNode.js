32fdde9beae9071683d3d421ed3f07d7448af33e
Daniel Petro <DanielPetro@nbp-153-116.nbp.ruw.rutgers.edu>
2016-01-19 20:26:20 -0500

    added a variable spotifyData to get the song name and artist name

--- liriNode.js
+++ liriNode.js
@@ -10,9 +10,9 @@ var spotify = require("spotify");
 
 
 switch(params[0]){
-//   case "movie-this":
-//     getMovie();
-//     break;
+  case "movie-this":
+    getMovie();
+    break;
 
    case "spotify-this":
     getSpotify();
@@ -24,37 +24,37 @@ switch(params[0]){
 }
 
 
-// //Movie Function to search for specific movies. If you don't put a movie in after movie-this it always defaults to Mr Nobody.
-// function getMovie() {
-//   if (params[1] === undefined) {
-//     request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&r=json&tomatoes=true', function (error, response, body){
-//         if (!error && response.statusCode == 200) {
-//           var movieData = "Title: " + JSON.parse(body)["Title"] + "\r\n" +
-//                                         "Year: " + JSON.parse(body)["Year"] + "\r\n" +
-//                                         "About: " + JSON.parse(body)["Plot"] + "\r\n" +
-//                                         "IMDB Rating: " + JSON.parse(body)["imdbRating"] + "\r\n" +
-//                                         "Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"] + "\r\n" +
-//                                         "Genre: " + JSON.parse(body)["Genre"];
-//           console.log(movieData);
-//           return;
-//         }      
-//     })
-//   } else {
-//     request('http://www.omdbapi.com/?t=' + params[1] + '&y=&plot=short&r=json&tomatoes=true', function (error, response, body){
-//         if (!error && response.statusCode == 200) {
-//           var movieData = "Title: " + JSON.parse(body)["Title"] + "\r\n" +
-//                                         "Year: " + JSON.parse(body)["Year"] + "\r\n" +
-//                                         "About: " + JSON.parse(body)["Plot"] + "\r\n" +
-//                                         "IMDB Rating: " + JSON.parse(body)["imdbRating"] + "\r\n" +
-//                                         "Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"] + "\r\n" +
-//                                         "Genre: " + JSON.parse(body)["Genre"];
-//           console.log(movieData);
-//           return;
-//         }      
-//     })          
-//   }
-// }
-// // end of Movie Function
+//Movie Function to search for specific movies. If you don't put a movie in after movie-this it always defaults to Mr Nobody.
+function getMovie() {
+  if (params[1] === undefined) {
+    request('http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&r=json&tomatoes=true', function (error, response, body){
+        if (!error && response.statusCode == 200) {
+          var movieData = "Title: " + JSON.parse(body)["Title"] + "\r\n" +
+                                        "Year: " + JSON.parse(body)["Year"] + "\r\n" +
+                                        "About: " + JSON.parse(body)["Plot"] + "\r\n" +
+                                        "IMDB Rating: " + JSON.parse(body)["imdbRating"] + "\r\n" +
+                                        "Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"] + "\r\n" +
+                                        "Genre: " + JSON.parse(body)["Genre"];
+          console.log(movieData);
+          return;
+        }      
+    })
+  } else {
+    request('http://www.omdbapi.com/?t=' + params[1] + '&y=&plot=short&r=json&tomatoes=true', function (error, response, body){
+        if (!error && response.statusCode == 200) {
+          var movieData = "Title: " + JSON.parse(body)["Title"] + "\r\n" +
+                                        "Year: " + JSON.parse(body)["Year"] + "\r\n" +
+                                        "About: " + JSON.parse(body)["Plot"] + "\r\n" +
+                                        "IMDB Rating: " + JSON.parse(body)["imdbRating"] + "\r\n" +
+                                        "Rotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"] + "\r\n" +
+                                        "Genre: " + JSON.parse(body)["Genre"];
+          console.log(movieData);
+          return;
+        }      
+    })          
+  }
+}
+// end of Movie Function
 
 function getSpotify() {
   if (params[1] === undefined){
@@ -63,37 +63,35 @@ function getSpotify() {
         console.log('Error occurred: ' + err);
         return;
     }
-    console.log(data.tracks.items[0].name);
-    console.log(data.tracks.items[0].artists[0].name);
+    var spotifyData = "Song Name: " + data.tracks.items[0].name + "\r\n" + 
+                                "Artist: " + data.tracks.items[0].artists[0].name;
+
+       console.log(spotifyData);
+  //   console.log(data.tracks.items[0].name); // gives the song name
+  //   console.log(data.tracks.items[0].artists[0].name); // gives the artist name
+  // // console.log(data.tracks.items[0].name);
+  // // console.log(data.tracks.items[0].name);
+});
+  } else {
+    spotify.search({ type: 'track', query: params[1] }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    console.log(data.tracks.items[0].name); // gives the song name
+    console.log(data.tracks.items[0].artists[0].name); // gives the artist name
   // console.log(data.tracks.items[0].name);
   // console.log(data.tracks.items[0].name);
 });
   }
+
+
 }
 
 
 
 
 
-
-
-
-
-
-// function getSpotify() {
-//   if (params[1] === undefined) {
-//     spotify.search({ type: 'track', query: "What's My Age Again" }, function(err, data) {
-//     if ( err ) {
-
-//         console.log('Error occurred: ' + err);
-//         console.log(data.tracks.album);
-//         return;
-//     }
- 
-//     // Do something with 'data' 
-// });
-//   }
-// }
 
 
 
